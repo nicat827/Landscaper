c3a825de626a2cfb4df8e64cf36ded5701e90f02
YunisAslan <yunisaslan.dev@gmail.com>
2024-01-23 14:26:03 +0400

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Landscaper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
